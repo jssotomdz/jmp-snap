c273d0fa5f72f65d9f1858a96772a50cc895ddb7
Jesús Soto <jesus.soto@canonical.com>
2023-01-10 16:30:53 -0600

    initial commit, v1.7.1 from binaries

---
+++ usr/share/jellyfinmediaplayer/web-client/extension/jmpInputPlugin.js
@@ -0,0 +1,38 @@
+const remap = {
+    "play_pause": "playpause",
+    "seek_forward": "fastforward",
+    "seek_backward": "rewind",
+    "host:fullscreen": "togglefullscreen",
+    "cycle_audio": "changeaudiotrack",
+    "cycle_subtitles": "changesubtitletrack",
+    "increase_volume": "volumeup",
+    "decrease_volume": "volumedown",
+    "step_backward": "previouschapter",
+    "step_forward": "nextchapter",
+    "enter": "select",
+}
+
+class jmpInputPlugin {
+    constructor({ inputManager }) {
+        this.name = 'JMP Input Plugin';
+        this.type = 'input';
+        this.id = 'jmpInputPlugin';
+
+        (async () => {
+            const api = await window.apiPromise;
+
+            api.input.hostInput.connect((actions) => {
+                actions.forEach(action => {
+                    if (remap.hasOwnProperty(action)) {
+                        action = remap[action];
+                    }
+                    inputManager.handleCommand(action, {});
+                });
+            });
+
+            api.system.hello("jmpInputPlugin");
+        })();
+    }
+}
+
+window._jmpInputPlugin = jmpInputPlugin;
---
+++ usr/share/jellyfinmediaplayer/web-client/extension/jmpUpdatePlugin.js
@@ -0,0 +1,46 @@
+class jmpUpdatePlugin {
+    constructor({ confirm, toast }) {
+        this.name = 'JMP Update Plugin';
+        this.type = 'input';
+        this.id = 'jmpUpdatePlugin';
+
+        (async () => {
+            const api = await window.apiPromise;
+
+            const onUpdateNotify = async (url) => {
+                if (url == "SSL_UNAVAILABLE") {
+                    // Windows (and possibly macOS) don't ship with SSL in QT......
+                    // So we get to do a full request to GitHub here :(
+                    const checkUrl = "https://github.com/jellyfin/jellyfin-media-player/releases/latest";
+                    url = (await fetch(checkUrl)).url;
+                }
+
+                const urlSegments = url.split("/");
+                const version = urlSegments[urlSegments.length - 1].substring(1);
+                const currentVersion = navigator.userAgent.split(" ")[1];
+
+                if (currentVersion.includes('pre')) return; // Do not notify for prereleases
+                if (version == currentVersion) return;
+                if (!/^[0-9.-]+$/.test(version)) return;
+
+                try {
+                    await confirm({
+                        title: "Update Available",
+                        text: `Jellyfin Media Player version ${version} is available.`,
+                        cancelText: "Ignore",
+                        confirmText: "Download"
+                    });
+
+                    api.system.openExternalUrl(url);
+                } catch (e) {
+                    // User cancelled update
+                }
+            }
+
+            api.system.updateInfoEmitted.connect(onUpdateNotify);
+            api.system.checkForUpdates();
+        })();
+    }
+}
+
+window._jmpUpdatePlugin = jmpUpdatePlugin;
